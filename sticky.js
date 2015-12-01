ca951e649d9eda0951af5a3abdc713f08c38d777
Kyle Burke <kyle@massroots.com>
2015-12-01 10:49:43 -0700

    Make directive minification safe, add check for stickyImmediate attribute that sticks the element as soon as scroll position is not zero.

--- sticky.js
+++ sticky.js
@@ -1,63 +1,89 @@
-angular.module("sticky", []).directive("sticky", function($window) {
-  return {
-    link: function(scope, element, attrs) {
+(function() {
+  angular
+  .module("sticky", [])
+  .directive("sticky", StickyDirective);
 
-      var $win = angular.element($window);
+  StickyDirective.$inject = ['$window'];
 
-      if (scope._stickyElements === undefined) {
-        scope._stickyElements = [];
+  function StickyDirective($window) {
+    return {
+      link: function(scope, element, attrs) {
 
-        $win.bind("scroll.sticky", function(e) {
-          var pos = $win.scrollTop();
-          for (var i=0; i<scope._stickyElements.length; i++) {
+        var $win = angular.element($window);
 
-            var item = scope._stickyElements[i];
+        if (scope._stickyElements === undefined) {
+          scope._stickyElements = [];
 
-            if (!item.isStuck && pos > item.start) {
-              item.element.addClass("stuck");
-              item.isStuck = true;
+          $win.bind("scroll.sticky", function(e) {
+            var pos = $win.scrollTop();
+            for (var i=0; i<scope._stickyElements.length; i++) {
 
-              if (item.placeholder) {
-                item.placeholder = angular.element("<div></div>")
-                    .css({height: item.element.outerHeight() + "px"})
-                    .insertBefore(item.element);
+              var item = scope._stickyElements[i];
+
+              if (attrs.stickyImmediate !== undefined) {
+                if (!item.isStuck && post > 0) {
+                  stickElement(item);
+                }
+                else if (item.isStuck && post <= 0) {
+                  unStickElement(item);
+                }
+              }
+              else {
+                if (!item.isStuck && pos > item.start) {
+                  stickElement(item);
+                }
+                else if (item.isStuck && pos < item.start) {
+                  unStickElement(item);
+                }
               }
             }
-            else if (item.isStuck && pos < item.start) {
-              item.element.removeClass("stuck");
-              item.isStuck = false;
+          });
 
-              if (item.placeholder) {
-                item.placeholder.remove();
-                item.placeholder = true;
+          var recheckPositions = function() {
+            for (var i=0; i<scope._stickyElements.length; i++) {
+              var item = scope._stickyElements[i];
+              if (!item.isStuck) {
+                item.start = item.element.offset().top;
+              } else if (item.placeholder) {
+                item.start = item.placeholder.offset().top;
               }
             }
-          }
-        });
+          };
+          $win.bind("load", recheckPositions);
+          $win.bind("resize", recheckPositions);
+        }
 
-        var recheckPositions = function() {
-          for (var i=0; i<scope._stickyElements.length; i++) {
-            var item = scope._stickyElements[i];
-            if (!item.isStuck) {
-              item.start = item.element.offset().top;
-            } else if (item.placeholder) {
-              item.start = item.placeholder.offset().top;
-            }
-          }
+        var item = {
+          element: element,
+          isStuck: false,
+          placeholder: attrs.usePlaceholder !== undefined,
+          start: element.offset().top
         };
-        $win.bind("load", recheckPositions);
-        $win.bind("resize", recheckPositions);
+
+        scope._stickyElements.push(item);
+
       }
+    };
+  }
 
-      var item = {
-        element: element,
-        isStuck: false,
-        placeholder: attrs.usePlaceholder !== undefined,
-        start: element.offset().top
-      };
+  function stickElement() {
+    item.element.addClass("stuck");
+    item.isStuck = true;
 
-      scope._stickyElements.push(item);
-
+    if (item.placeholder) {
+      item.placeholder = angular.element("<div></div>")
+          .css({height: item.element.outerHeight() + "px"})
+          .insertBefore(item.element);
     }
-  };
-});
+  }
+
+  function unStickElement(item) {
+    item.element.removeClass("stuck");
+    item.isStuck = false;
+
+    if (item.placeholder) {
+      item.placeholder.remove();
+      item.placeholder = true;
+    }
+  }
+})();
